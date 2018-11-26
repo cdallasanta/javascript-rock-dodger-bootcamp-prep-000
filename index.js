31b9ce9d0a9ca623301e647dd351accabed7051e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-26 19:19:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ function endGame() {
   //visually removes rocks, but they are still in the background falling
   ROCKS.forEach(function(element){
     element.remove();
-    element.style.top = "401px";
     element.style.left = "401px";
   });
   
